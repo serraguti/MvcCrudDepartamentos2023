27664cba43d8851bc0964d1c450f427f4f0b4976
serraguti <pacoserranox@gmail.com>
2023-01-17 19:33:54 +0100

    commit

--- MvcCrudDepartamentos/Controllers/DepartamentosController.cs
+++ MvcCrudDepartamentos/Controllers/DepartamentosController.cs
@@ -19,5 +19,22 @@ namespace MvcCrudDepartamentos.Controllers
             List<Departamento> departamentos = this.repo.GetDepartamentos();
             return View(departamentos);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        //VAMOS A RECIBIR TODO EL OBJETO DEPARTAMENTO
+        //TENEMOS DOS OPCIONES:
+        //1) RECIBIMOS TODO EL OBJETO
+        //2) RECIBIMOS CADA VARIABLE
+        [HttpPost]
+        public IActionResult Create(Departamento dept)
+        {
+            this.repo.InsertDepartamento(dept.IdDepartamento, dept.Nombre, dept.Localidad);
+            //EN LUGAR DE MOSTRAR UN MENSAJE O ALGO, LO QUE VAMOS A REALIZAR
+            //ES VOLVER A LA VISTA DONDE ESTAN TODOS LOS DEPARTAMENTOS (Index)
+            return RedirectToAction("Index");
+        }
     }
 }
